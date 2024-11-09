918df4ac857ab0d1890db08788d303a400fde43d
Aylin Oncel <aylinoncel@MacBook-Pro.local>
2024-11-09 23:17:47 +0300

    Added old files

---
+++ JavaProjects/Intro to Java/FahrenheitToCelsius.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Fahrenheit value: ");
+        int fahrenheit = input.nextInt();
+        input.nextLine(); //cleans up newLine at the end of user input
+        System.out.print("Enter a date in format of DayOfWeek Month Day: ");  
+        String day = input.nextLine();
+        double celsius = (5.0/9) * (fahrenheit - 32);
+        System.out.println(day + " Fahrenheit: " + fahrenheit);
+        System.out.println(day + " Celsius: " + celsius);
+    }
+}
